dc04182b770d1b14590b0b0a5f918579f3599987
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-14 20:47:19 +0530

    Create AddressBook Service Table

--- AddressBook.sql
+++ AddressBook.sql
@@ -3,3 +3,16 @@ create database AddressBookService;
 -- View database name
 use AddressBookService;
 select DB_NAME() 
+
+-- Create table in the database
+create table Address_Book
+(
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+Address varchar(60) not null,
+City varchar(15) not null,
+State varchar(15) not null,
+Zipcode varchar(6) not null,
+PhoneNumber varchar(12) not null,
+Email varchar(25) not null
+);
